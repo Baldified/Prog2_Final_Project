ee825e4892d1bddaaf9ed8ee9dc536161be167f2
MP-VC <2519989@edu.vaniercollege.qc.ca>
2026-04-27 10:59:57 -0400

    main method

---
+++ Main.java
@@ -0,0 +1,40 @@
+public class Main {
+    public static void main(String[] args) {
+        Division d = new Division();
+        PatrolOfficer po = new PatrolOfficer("employeeId", "firstName",  "lastName",
+                           2,  "hireDate",  "address",  "phoneNumber",
+                           d.getDivisionName(),  1, 30.00,
+                           5,  4,  3,"Leuitenent", 5, "Monday");
+        Detective dec = new Detective();
+        Dispatcher dis = new Dispatcher();
+        SwornOfficer swo = new SwornOfficer();
+
+        po.setEmployeeId("p1");
+        po.setFirstName("bob");
+        po.setLastName("cob");
+
+        dec.setEmployeeId("dec1");
+        dec.setFirstName("bib");
+        dec.setLastName("cib");
+
+        dis.setEmployeeId("dis1");
+        dis.setFirstName("beb");
+        dis.setLastName("ceb");
+
+        swo.setEmployeeId("swo1");
+        swo.setFirstName("bub");
+        swo.setLastName("cub");
+
+        d.addEmployee(po);
+        d.addEmployee(dec);
+        d.addEmployee(dis);
+        d.addEmployee(swo);
+
+        System.out.println(d.getEmployees());
+        Division dcopy = d;
+        System.out.println(d.equals(dcopy));
+        d.setSupervisor(po);
+        dcopy.setSupervisor(swo);
+        System.out.println(d.getSupervisor().equals(dcopy.getSupervisor()));
+    }
+}
--- PatrolOfficer.java
+++ PatrolOfficer.java
@@ -1,4 +1,11 @@
 public class PatrolOfficer extends SwornOfficer {
+    
+    public PatrolOfficer(String employeeId, String firstName, String lastName, int age, String hireDate, String address,
+            String phoneNumber, String divisionName, int yearsOfService, double baseSalary, int trainingScore,
+            int overtimeHours, int disciplineRecordCount, String rank, double hazardAllowance, String shift) {
+        super(employeeId, firstName, lastName, age, hireDate, address, phoneNumber, divisionName, yearsOfService,
+                baseSalary, trainingScore, overtimeHours, disciplineRecordCount, rank, hazardAllowance, shift);
+    }
     public PatrolOfficer() { super(); }
     public PatrolOfficer(SwornOfficer other) { super(other); }
 }
