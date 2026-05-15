9ef097f0682ec594e2ddf80688ef440cfe66d902
bruce <l4m.bruc3@gmail.com>
2026-05-14 21:32:38 -0400

    Added Overrides

--- PatrolOfficer.java
+++ PatrolOfficer.java
@@ -1,3 +1,6 @@
+
+import java.util.Objects;
+
 public class PatrolOfficer extends SwornOfficer {
     
     public PatrolOfficer(String employeeId, String firstName, String lastName, int age, String hireDate, String address,
@@ -8,4 +11,72 @@ public class PatrolOfficer extends SwornOfficer {
     }
     public PatrolOfficer() { super(); }
     public PatrolOfficer(SwornOfficer other) { super(other); }
+
+    @Override
+    public String toString() {
+        return employeeId + " " + firstName + " " + lastName + " " + getRoleCategory();
+    }
+
+    @Override
+    public boolean equals(Object person) {
+        if (this == person)
+        return true;
+    if (person == null)
+        return false;
+    if (getClass() != person.getClass())
+        return false;
+    
+    final Person other = (Person) person;
+
+    if (!Objects.equals(this.employeeId, other.employeeId))
+        return false;
+    if (!Objects.equals(this.firstName, other.firstName))
+        return false;
+     if (!Objects.equals(this.lastName, other.lastName))
+        return false;
+    if (this.age != other.age)
+        return false;
+    if (!Objects.equals(this.hireDate, other.hireDate))
+        return false;
+    if (!Objects.equals(this.phoneNumber, other.phoneNumber))
+        return false;
+    return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash += 53 * hash + Objects.hashCode(this.employeeId);
+        hash += 7 * hash + Objects.hashCode(this.firstName);
+        hash += 31 * hash + Objects.hashCode(this.lastName);
+        hash += 53 * hash + Objects.hashCode(this.hireDate);
+        hash += 41 * hash + Objects.hashCode(this.age);
+        hash += 7 * hash + Objects.hashCode(this.phoneNumber);
+        return hash;
+    }
+
+    @Override
+    public String getShift() {
+        return shift;
+    }
+
+    @Override
+    public void assignShift(String string) {
+        this.shift = string;
+    }
+
+    @Override
+    public double calculateMonthlyCompensation() {
+        return (this.baseSalary * 8 * 30) + (this.overtimeHours * this.baseSalary * 2);
+    }
+
+    @Override
+    public String getRoleCategory() {
+        return "Patrol Officer";
+    }
+
+    @Override
+    public double computePayRoll() {
+        return calculateMonthlyCompensation() + (this.overtimeHours * this.baseSalary * 2);
+    }
 }
