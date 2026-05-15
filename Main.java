7c8d4b34c19e45b3e319f6ac3e0769a674b826a3
MP-VC <2519989@edu.vaniercollege.qc.ca>
2026-05-14 22:07:59 -0400

    f

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InvalidSupervisorAssignmentException {
          Division de = new Division();
          de.setDivisionName("Ghost");
         
